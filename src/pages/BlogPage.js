41f0d2613e09e5262e735dffe9340e514aae3d2c
Charles Daab <charlesjdaab@gmail.com>
2020-06-20 08:21:56 -0400

    typo

--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8a35a671456eb8a9ab1aced81903f8a6128dd90c6a895711ea5d7c529dae823
-size 5499
+oid sha256:058bdd031c16dc9798962157b81f6ea344d1ffcbe8a13f4f8d11ad27b6679fa4
+size 5494
