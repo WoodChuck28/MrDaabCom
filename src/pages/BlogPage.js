8ca0988428630532ea71a182b81f914cd6433a0c
Charles Daab <charlesjdaab@gmail.com>
2020-06-21 06:52:24 -0400

    changed image names

--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cee53714f8d02be61b72425289c5dd57c57b5cb158fa4b62832d9f5b7f84a5c9
-size 5568
+oid sha256:63f57da2b6f0520c587b70483a8ba06b58ffdb0863bc529bd00708663d2db8b6
+size 5564
