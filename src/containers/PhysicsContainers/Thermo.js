71bff2e4790afb536dbdb232917ed501656b180a
Charles Daab <charlesjdaab@gmail.com>
2020-06-28 11:40:25 -0400

    removed warning

--- src/containers/PhysicsContainers/Thermo.js
+++ src/containers/PhysicsContainers/Thermo.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7b5b7df6b73a71e875f12d47e789e50a692de7ff5ef1f0c824589e4d9b4db65
-size 1849
+oid sha256:d9bfc66bcf6eb3c017ea3a757cefb4343e728bfb6f8cd8be855dcd33b91b7fc2
+size 1838
