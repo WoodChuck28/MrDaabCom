a4ee22e3aecddb234d5d6f4b781711387f4933af
Charles Daab <charlesjdaab@gmail.com>
2020-06-28 11:41:19 -0400

    another error needed removing

--- src/pages/ComputerScience.js
+++ src/pages/ComputerScience.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d9096c35ce3cfbcbffdfc6b09f02c2ab2cb8b8df8fc2f1c8f4cbdc27089af663
-size 1344
+oid sha256:01faa482c33d18c87254116a731d63b5124bdf0a43ef82b90ef43d94bbf941d5
+size 1331
