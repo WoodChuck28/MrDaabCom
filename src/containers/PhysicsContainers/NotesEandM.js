e46a3debf320c5fe6029ef666485f6659403203c
Charles Daab <charlesjdaab@gmail.com>
2020-12-26 07:19:23 -0500

    mag field image

--- src/containers/PhysicsContainers/NotesEandM.js
+++ src/containers/PhysicsContainers/NotesEandM.js
@@ -5,7 +5,7 @@ class NotesEandM extends Component {
   render() {
     return (
       <div>
-      
+        <img src="/src/assets/resources/img/magnetic_field_2.jpg"></img>
       </div>
     );
   }
