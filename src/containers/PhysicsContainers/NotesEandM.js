b1b96b6082ce63871fbcc7edbbd6c87e1812de20
Charles Daab <charlesjdaab@gmail.com>
2020-12-26 07:24:38 -0500

    alt txt

--- src/containers/PhysicsContainers/NotesEandM.js
+++ src/containers/PhysicsContainers/NotesEandM.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9102ce252b8852ac4d35f62fef7ada56fb0d6df94aa7653a677da7d2b28e7afe
-size 246
+oid sha256:b1dba0780b06658fc9212bc9d3520440a7aef1ef6fbfdb1fdce77c13d3388d07
+size 259
