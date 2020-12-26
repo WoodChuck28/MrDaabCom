0b5142a37e36a335a7611fce1a192c5a83c9721d
Charles Daab <charlesjdaab@gmail.com>
2020-12-26 07:06:46 -0500

    test

--- src/containers/PhysicsContainers/NotesEandM.js
+++ src/containers/PhysicsContainers/NotesEandM.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 
-class NotesDyn extends Component {
+class NotesEandM extends Component {
   render() {
     return (
       <div>
@@ -11,4 +11,4 @@ class NotesDyn extends Component {
   }
 }
  
-export default NotesDyn;
+export default NotesEandM;
