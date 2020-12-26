13cb0294d691cfb902a5bf8c324279cce2127f4f
Charles Daab <charlesjdaab@gmail.com>
2020-12-26 07:29:13 -0500

    img update

--- src/containers/PhysicsContainers/NotesEandM.js
+++ src/containers/PhysicsContainers/NotesEandM.js
@@ -5,7 +5,12 @@ class NotesEandM extends Component {
   render() {
     return (
       <div>
-        <img src="/src/assets/resources/img/magnetic_field_2.jpg" alt="magnet"></img>
+        <header>
+          <img src="https://bl6pap003files.storage.live.com/y4mZpvHsRccINrV-P5jbSZjDfvmx6Qs0OSD9L_mGo_woCl11QpwU6nwMqVEgz4H1IykZVnz-l33UdsaDB9bQgLD9imE5qef_U-dxkaOUzee3P-do7bdX8vFRZYMOxazV0AoZJjNEcSHckMCzWrxmXpDffS11zL59RC4DaB2hZHAleoym9bkDoWzIpVfQqrAJBSn?width=445&height=326&cropmode=none" 
+          alt="magnet"></img>
+          <h1>Magnetic Systems, Fields, and Forces</h1>
+          <h3>Lets learn how the electric field relates to the magnetic force field!</h3>
+          </header>
       </div>
     );
   }
