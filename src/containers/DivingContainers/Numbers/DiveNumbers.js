b51e0d47773d0fcb110446005fd22b8f78f0af64
Charles Daab <charlesjdaab@gmail.com>
2021-07-13 09:46:02 -0400

    teh

--- src/containers/DivingContainers/Numbers/DiveNumbers.js
+++ src/containers/DivingContainers/Numbers/DiveNumbers.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-import Iframe from 'react-iframe'
+import Iframe from 'react-iframe';
+import "../../styles/CollegeBoardDoc.css";
 
 class DivingNumbers extends Component {
     render() {
       return (
-        <div className="myDivingClass">
+        <div className="mysyntaxClass">
         <Iframe src="https://onedrive.live.com/embed?cid=453322899BA6D77B&resid=453322899BA6D77B%21143708&authkey=AP1FCRVZwsXl1O0&em=2" 
         width="100%" height="500" frameborder="0" scrolling="no"></Iframe>
         </div>
