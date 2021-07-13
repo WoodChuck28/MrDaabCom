42d39ca7ca728ea642d1c1b1d561bc86f0f3fe6f
Charles Daab <charlesjdaab@gmail.com>
2021-07-13 09:23:44 -0400

    IFrame

--- src/containers/DivingContainers/Dives/SubDirs/101b.js
+++ src/containers/DivingContainers/Dives/SubDirs/101b.js
@@ -6,8 +6,8 @@ class frontPike extends Component {
   render() {
     return (
       <div className="myDivingClass">
-      <iframe src="https://onedrive.live.com/embed?cid=453322899BA6D77B&resid=453322899BA6D77B%211195&authkey=ADggslUcS-LV_JA" 
-      width="500px" height="100%" frameborder="0" scrolling="no" allowfullscreen></iframe>
+      <Iframe src="https://onedrive.live.com/embed?cid=453322899BA6D77B&resid=453322899BA6D77B%211195&authkey=ADggslUcS-LV_JA" 
+      width="500px" height="100%" frameborder="0" scrolling="no" allowfullscreen></Iframe>
       </div>
     );
   }
