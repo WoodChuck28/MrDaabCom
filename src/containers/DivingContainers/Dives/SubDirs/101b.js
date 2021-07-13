998287c410e25301b330eb968b07316739ea51eb
Charles Daab <charlesjdaab@gmail.com>
2021-07-13 13:04:30 -0400

    added youtube embed component

--- src/containers/DivingContainers/Dives/SubDirs/101b.js
+++ src/containers/DivingContainers/Dives/SubDirs/101b.js
@@ -1,16 +1,16 @@
 import React, { Component } from "react";
-import Iframe from 'react-iframe'
+import YoutubeEmbed from "../../../../components/youTubeEmbed";
 import "../../../styles/CollegeBoardDoc.css";
  
 class frontPike extends Component {
   render() {
     return (
-      <div className="myDivingClass">
-      <Iframe src="https://onedrive.live.com/embed?cid=453322899BA6D77B&resid=453322899BA6D77B%211195&authkey=ADggslUcS-LV_JA" 
-      width="100%" height="500" frameborder="0" scrolling="no" ></Iframe>
+        <div className="App">
+        <h1>Youtube Embed</h1>
+        <YoutubeEmbed embedId="2KTgZ5C0Sis" />
       </div>
     );
   }
 }
- 
+
 export default frontPike;
