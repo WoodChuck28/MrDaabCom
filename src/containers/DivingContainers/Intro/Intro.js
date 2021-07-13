74d9755d8e6869166c0669349e5b0cbe8d6a0339
Charles Daab <charlesjdaab@gmail.com>
2021-07-13 08:44:57 -0400

    More dive page edits

--- src/containers/DivingContainers/Intro/Intro.js
+++ src/containers/DivingContainers/Intro/Intro.js
@@ -4,10 +4,12 @@ import {
   HashRouter,
   NavLink
 } from "react-router-dom";
-import PythonSyntax from "./PythonSyntax";
-import PythonWhen from "./PythonWhen";
-import { Card } from "@material-ui/core";
 
+import DivingHistory from "./SubDirs/History";
+import DivingCompetition from "./SubDirs/Competition";
+import DivingSuccess from "./SubDirs/Success";
+import DivingExamples from "./SubDirs/Examples";
+import { Card } from "@material-ui/core";
 
 class DivingIntro extends Component {
   render() {
@@ -16,17 +18,17 @@ class DivingIntro extends Component {
       <div>
       <h2 style={{textAlign: "center", marginTop: "70px"}}>Python - Super High Level Programming Language</h2>
       <ul className="header" style={{background: "#001a00", height: "70px"}}>
-      <li><NavLink to="/pythonSyntax" style={{color: "white"}}>History of Sport</NavLink></li>
-      <li><NavLink to="/pythonUsage" style={{color: "white"}}>How Competition Works</NavLink></li>
-      <li><NavLink to="/pythonFrameworks" style={{color: "white"}}>Who is Successful?</NavLink></li>
-      <li><NavLink to="/myPythonExamples" style={{color: "white"}}>Diving Examples</NavLink></li>
+      <li><NavLink to="/divingHistory" style={{color: "white"}}>History of Sport</NavLink></li>
+      <li><NavLink to="/divingCompetition" style={{color: "white"}}>How Competition Works</NavLink></li>
+      <li><NavLink to="/divingSuccess" style={{color: "white"}}>Who is Successful?</NavLink></li>
+      <li><NavLink to="/divingExamples" style={{color: "white"}}>Diving Examples</NavLink></li>
       </ul>
         
         <div className="content">
-          <Route path="/pythonSyntax" component={PythonSyntax}/>
-          <Route path="/pythonUsage" component={PythonWhen}/>
-          <Route path="/pythonFrameworks" component={PythonSyntax}/>
-          <Route path="/myPythonExamples" component={PythonSyntax}/>
+          <Route path="/divingHistory" component={DivingHistory}/>
+          <Route path="/divingCompetition" component={DivingCompetition}/>
+          <Route path="/divingSuccess" component={DivingSuccess}/>
+          <Route path="/divingExamples" component={DivingExamples}/>
         </div>
         <Card style={{textAlign: "center", background: "inherit"}}>
           <img src="https://th.bing.com/th/id/OIP.Po8oq2jSJwuupdDRIMfxHQHaHa?w=167&h=180&c=7&o=5&dpr=1.25&pid=1.7" alt="python"
---
+++ src/containers/DivingContainers/Intro/SubDirs/History.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Iframe from 'react-iframe'
+import "../../styles/CollegeBoardDoc.css";
+ 
+class DivingHistory extends Component {
+  render() {
+    return (
+      <div className="myDivingClass">
+      <Iframe src="https://onedrive.live.com/embed?cid=453322899BA6D77B&resid=453322899BA6D77B%21133873&authkey=AGTnDJJltOzZ5Js&em=2" 
+      width="100%" height="500" frameborder="0" scrolling="yes"></Iframe>
+      </div>
+    );
+  }
+}
+ 
+export default DivingHistory;
