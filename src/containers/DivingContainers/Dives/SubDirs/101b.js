c6a4db439024fb800df0058beaeedea82dfd9f59
Charles Daab <charlesjdaab@gmail.com>
2021-07-13 12:50:43 -0400

    divingclass css

--- src/containers/DivingContainers/Dives/SubDirs/101b.js
+++ src/containers/DivingContainers/Dives/SubDirs/101b.js
@@ -7,7 +7,7 @@ class frontPike extends Component {
     return (
       <div className="myDivingClass">
       <Iframe src="https://onedrive.live.com/embed?cid=453322899BA6D77B&resid=453322899BA6D77B%211195&authkey=ADggslUcS-LV_JA" 
-      width="100%" height="500" frameborder="0" scrolling="no" allowfullscreen></Iframe>
+      width="100%" height="500" frameborder="0" scrolling="no" ></Iframe>
       </div>
     );
   }
