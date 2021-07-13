de55f468a432f1d05c7c5f087f397d7c08e64a32
Charles Daab <charlesjdaab@gmail.com>
2021-07-13 09:49:22 -0400

    padding

--- src/containers/DivingContainers/Numbers/DiveNumbers.js
+++ src/containers/DivingContainers/Numbers/DiveNumbers.js
@@ -5,7 +5,7 @@ import "../../styles/CollegeBoardDoc.css";
 class DivingNumbers extends Component {
     render() {
       return (
-        <div className="mysyntaxClass">
+        <div className="mysyntaxClass" style={{paddingTop = "25px"}}>
         <Iframe src="https://onedrive.live.com/embed?cid=453322899BA6D77B&resid=453322899BA6D77B%21143708&authkey=AP1FCRVZwsXl1O0&em=2" 
         width="100%" height="500" frameborder="0" scrolling="no"></Iframe>
         </div>
