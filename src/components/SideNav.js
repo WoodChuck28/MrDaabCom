666896d6d7da6f1e35035104d5eedc8588756a0d
Charles Daab <charlesjdaab@gmail.com>
2021-07-14 08:17:31 -0400

    props.name added

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:378e631fec697b11633e7c6bf4853ada46d94e25540b2eb02d0d4c19c30680e3
-size 294
+oid sha256:49301df59ac48e9026f12f66e83d12c1340c2fdc18c201b9162226ea2c2b3e56
+size 379
