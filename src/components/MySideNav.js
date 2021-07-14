d77039e4ffbedb6caa429e98b0a840ff7bd04c29
Charles Daab <charlesjdaab@gmail.com>
2021-07-14 12:17:08 -0400

    beh

--- src/components/MySideNav.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b62dee1aa0fce975c1c1bcb67205fba97dc3f967087eaa5f73860fe20f90ee70
-size 1822
--- src/pages/Physics.js
+++ src/pages/Physics.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ca2d9de115efe93789b3fecc5e6a9f7b76d972fd06e848a330bd29b04463aa4
-size 3193
+oid sha256:3d8c117ccca0c1b800b32fca0e088652f6c8a1d7acb7075e3f7cbde9874fb9d4
+size 1841
