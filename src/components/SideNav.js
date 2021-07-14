94dbbbec5e3e92a3acc02fff8912162595a55cf4
Charles Daab <charlesjdaab@gmail.com>
2021-07-14 09:42:21 -0400

    removed sidenav

--- src/components/SideNav.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6932e228e5532a1a0928be2ee279852b2eff2da432bf10e1b318006cc208cb51
-size 660
--- src/pages/Physics.js
+++ src/pages/Physics.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:17fa9237c77128b5cd9c5d36ed91bad5a1f1d742bf77cde076de3f11d7be4611
-size 1851
+oid sha256:b66c5f4089b0351148a391017ad7a76f5d23274dd072e1544466c68ba7855651
+size 1687
