80599e41d727af2338acda67f14603f317a3fb19
Charles Daab <charlesjdaab@gmail.com>
2021-07-14 08:17:31 -0400

    props.name added

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -3,11 +3,12 @@ import "./styles/SideNav.css"
 
 const SideNav = (props) => {
 return (
-<div className='sidenav'>
+<div className='sidenav' style={{ width: "25%", paddingTop: "20px" }}>
    <a href='#section'>About</a>
    <a href='#section'>Services</a>
    <a href='#section'>Clients</a>
    <a href='#section'>Contact</a>
+   <a href="#section">{props.name}</a>
 </div>
  );
 };
