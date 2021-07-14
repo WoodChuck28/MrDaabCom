8786f4275175b09d2ffece69c7f7ff0906db4b7c
Charles Daab <charlesjdaab@gmail.com>
2021-07-14 09:12:18 -0400

    sidenav codes added

--- src/pages/Physics.js
+++ src/pages/Physics.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d3993eee9faed63875aeef4c69ed9fe41ace9ad9a8b0d20bb8269dcc2d4d216
-size 1845
+oid sha256:17fa9237c77128b5cd9c5d36ed91bad5a1f1d742bf77cde076de3f11d7be4611
+size 1851
