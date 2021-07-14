4f4de998768d80c16404adb077bf4906f72316cb
Charles Daab <charlesjdaab@gmail.com>
2021-07-14 12:06:59 -0400

    used function

--- src/components/MySideNav.js
+++ src/components/MySideNav.js
@@ -1,8 +1,9 @@
+import React, { useState } from 'react';
 import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
 
 // Be sure to include styles at some point, probably during your bootstraping
 import '@trendmicro/react-sidenav/dist/react-sidenav.css';
-export default function MySideNav() {
+export default function MySideNav(props) {
     return(
 <Router>
     <Route render={({ location, history }) => (
