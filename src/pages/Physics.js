ed03a10ec1fa6950170bb46a9d6b39431511fe91
Charles Daab <charlesjdaab@gmail.com>
2021-07-15 07:31:35 -0400

    moved header to above

--- src/pages/Physics.js
+++ src/pages/Physics.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85e976cf758a5543e9c88d51c9f08367144dafc3d56814e976fe27b14e744908
-size 1812
+oid sha256:54269e3ddd85f061b43b3aef34a0b543e8592ea75c978a2d24fc2251ae751b6a
+size 1765
