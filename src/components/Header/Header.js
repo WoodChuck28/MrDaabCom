457f53b48c3fae6f00012a24435fd6300e134cb5
Charles Daab <charlesjdaab@gmail.com>
2021-07-15 07:55:04 -0400

    css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -58,10 +58,10 @@ const Header = () => {
               <MenuItem active={true} icon={<FiHome />}>
                 Home
               </MenuItem>
-              <MenuItem icon={<FaList />}>Category</MenuItem>
-              <MenuItem icon={<FaRegHeart />}>Favourite</MenuItem>
-              <MenuItem icon={<RiPencilLine />}>Author</MenuItem>
-              <MenuItem icon={<BiCog />}>Settings</MenuItem>
+              <MenuItem icon={<FaList />}>Kinematics</MenuItem>
+              <MenuItem icon={<FaRegHeart />}>Dynamics</MenuItem>
+              <MenuItem icon={<RiPencilLine />}>Energy</MenuItem>
+              <MenuItem icon={<BiCog />}>Momentum</MenuItem>
             </Menu>
           </SidebarContent>
           <SidebarFooter>
