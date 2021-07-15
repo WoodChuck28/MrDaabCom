395582227baffb59661ba4d2a63fe9448086957f
Charles Daab <charlesjdaab@gmail.com>
2021-07-15 06:54:54 -0400

    npm install icons

--- src/containers/DivingContainers/Programs/Subdirs/DaabDiving.js
+++ src/containers/DivingContainers/Programs/Subdirs/DaabDiving.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import "../../../styles/CollegeBoardDoc.css";
+
 import SinglePage from "../../../../components/PDF/SinglePage";
+import AllPages from "../../../../components/PDF/AllPages";
+
+
 import DaabDive from "./daabdive.pdf";
  
 class DaabDiving extends Component {
@@ -14,7 +18,7 @@ class DaabDiving extends Component {
 
       <h4>All Pages</h4>
       <div className="all-page-container">
-        <AllPagesPDFViewer pdf={DaabDive} />
+        <AllPages pdf={DaabDive} />
       </div>
 
       <hr />
