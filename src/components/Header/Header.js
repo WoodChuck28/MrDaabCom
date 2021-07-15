fe39955f4a618a67aaf01eb3a78286e94b074418
Charles Daab <charlesjdaab@gmail.com>
2021-07-15 08:06:33 -0400

    removed fa icons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:61c2d45e91337de37f38857f2d362268d09559fffad4f25ed188ab1cbcd81785
-size 2349
+oid sha256:3b84bc5e29da866fcae98f5d92c38d51895c71af0f16842e09772ed7e1a9a622
+size 2192
