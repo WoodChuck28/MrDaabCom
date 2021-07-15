0277052c6975ea46a08c1d9aa66fbb8e6f8511d9
Charles Daab <charlesjdaab@gmail.com>
2021-07-15 07:29:35 -0400

    dive css

--- src/containers/DivingContainers/Programs/Subdirs/DaabDiving.js
+++ src/containers/DivingContainers/Programs/Subdirs/DaabDiving.js
@@ -1,26 +1,17 @@
 import React, { Component } from "react";
-import "../../../styles/CollegeBoardDoc.css";
+import "../../../styles/Diving.css";
 
 import SinglePage from "../../../../components/PDF/SinglePage";
-import AllPages from "../../../../components/PDF/AllPages";
 
 import DaabDive from "./daabdive.pdf"
 
 class DaabDiving extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="daabdive">
       <h4>Single Page</h4>
       <SinglePage pdf={DaabDive} />
 
-      <hr />
-
-      <h4>All Pages</h4>
-      <div className="all-page-container">
-        <AllPages pdf={DaabDive} />
-      </div>
-
-      <hr />
     </div>
     );
   }
