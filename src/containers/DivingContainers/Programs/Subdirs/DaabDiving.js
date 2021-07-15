b1bc51ffb8cf1e62466d5cd1c2cb505df1416f67
Charles Daab <charlesjdaab@gmail.com>
2021-07-15 06:48:27 -0400

    multi pages

--- src/containers/DivingContainers/Programs/Subdirs/DaabDiving.js
+++ src/containers/DivingContainers/Programs/Subdirs/DaabDiving.js
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
 import "../../../styles/CollegeBoardDoc.css";
 import SinglePage from "../../../../components/PDF/SinglePage";
-import DaabDivingPDF from "./DaabDiving.pdf";
+import DaabDive from "./daabdive.pdf";
  
 class DaabDiving extends Component {
   render() {
     return (
       <div className="App">
       <h4>Single Page</h4>
-      <SinglePage pdf={DaabDivingPDF} />
+      <SinglePage pdf={DaabDive} />
+
+      <hr />
+
+      <h4>All Pages</h4>
+      <div className="all-page-container">
+        <AllPagesPDFViewer pdf={DaabDive} />
+      </div>
+
+      <hr />
     </div>
     );
   }
