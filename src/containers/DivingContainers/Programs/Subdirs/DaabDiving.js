e76354704697c8979fc7522df6c6c7d578c81eb3
Charles Daab <charlesjdaab@gmail.com>
2021-07-15 06:36:52 -0400

    new pdf viewer

--- src/containers/DivingContainers/Programs/Subdirs/DaabDiving.js
+++ src/containers/DivingContainers/Programs/Subdirs/DaabDiving.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import "../../../styles/CollegeBoardDoc.css";
 import SinglePage from "../../../../components/PDF/SinglePage";
-import DaabDiving from "./DaabDiving.pdf";
+import DaabDivingPDF from "./DaabDiving.pdf";
  
 class DaabDiving extends Component {
   render() {
     return (
       <div className="App">
       <h4>Single Page</h4>
-      <SinglePage pdf={DaabDiving} />
+      <SinglePage pdf={DaabDivingPDF} />
     </div>
     );
   }
