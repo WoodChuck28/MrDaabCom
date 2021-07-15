e95ea3b117d88bb9dc1e5d2f89e19c6095de3e0c
Charles Daab <charlesjdaab@gmail.com>
2021-07-15 07:55:04 -0400

    css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd14d5dc0031dc148abe5ac63c14bbe917c61cedeaa458d45981ce0e382605e9
-size 2500
+oid sha256:6580ebaf29fac4b3bf5a0f5e7c6a21e61514ee8ea85f28c1f155b76361fcb5a9
+size 2501
