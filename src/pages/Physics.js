0cd5def672e628e829f03d652a68e987e1ea29b4
Charles Daab <charlesjdaab@gmail.com>
2021-07-15 09:26:03 -0400

    modified routes with path

--- src/pages/Physics.js
+++ src/pages/Physics.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ae508961afd96829f00023d1c4dbb9dbd00f54c048868a9631f0e979e8c9404
-size 1667
+oid sha256:a4e0236dda9a2e8d988669bbe48ae9408150e39ac39004bc519933fd97c6dd5a
+size 1614
