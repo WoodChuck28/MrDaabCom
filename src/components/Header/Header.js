a0019ed7e6f1765985a2fcdbf03a4ca0039c488b
Charles Daab <charlesjdaab@gmail.com>
2021-07-15 08:16:34 -0400

    trying nav routing in sidebar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b84bc5e29da866fcae98f5d92c38d51895c71af0f16842e09772ed7e1a9a622
-size 2192
+oid sha256:16b6d1c9f2759b28dfb136d24d30fb7ffec6dd0806394ad109dd46b113773b3b
+size 2436
