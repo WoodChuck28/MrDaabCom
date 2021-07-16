09777b5e8c915eb3af9a0ef78117e77568d06759
Charles Daab <charlesjdaab@gmail.com>
2021-07-16 07:29:52 -0400

    trying image in root folder

--- src/pages/SwimDive.js
+++ src/pages/SwimDive.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a26c5a9e99845be3f1504870cfe5fe9d6842d8f36a57b576bef2b9f77c75172
-size 1551
+oid sha256:4e9fd656ccd2fa507eb629982e14ed299643beb4713c6ae0e78cb4cb9da72691
+size 1504
