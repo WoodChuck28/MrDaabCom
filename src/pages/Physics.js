177670a3747caa0ab5f042a1c419443ad78af10b
Charles Daab <charlesjdaab@gmail.com>
2021-07-16 14:23:28 -0400

    ghe

--- src/pages/Physics.js
+++ src/pages/Physics.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:941566a5a001afa17a3e644dd8413fa61127ae8f7e6b76300ac36c615cbe1b21
-size 1729
+oid sha256:9028fe04f3946fa4ece0ab66212a822577c4a9dc1d453da935f0768c6511a34e
+size 510
