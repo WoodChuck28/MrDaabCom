921fea7eeb08393b559a3777059407e8c2ca9f46
Charles Daab <charlesjdaab@gmail.com>
2021-07-16 06:56:39 -0400

    moved header back

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:96a05394b5f820197415668403d9e43bd336f34a61edef9106ab42ef94237098
-size 2703
+oid sha256:eeb891cbfb58f121f82a0e65db6d3a1ee92b2b279c7b7e02cbea55a4eadefd2c
+size 2720
