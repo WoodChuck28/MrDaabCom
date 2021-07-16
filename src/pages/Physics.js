12390c84e42164414f3d4fb7bb22eb0ec2e63947
Charles Daab <charlesjdaab@gmail.com>
2021-07-16 14:23:28 -0400

    ghe

--- src/pages/Physics.js
+++ src/pages/Physics.js
@@ -1,47 +1,14 @@
 import React from "react";
 import "./styles/Physics.css";
-import {
-  Route,
-  HashRouter,
-  NavLink,
-} from "react-router-dom";
 
-import Dynamics from "../containers/PhysicsContainers/Dynamics";
-import Energy from "../containers/PhysicsContainers/Energy";
-import Momo from "../containers/PhysicsContainers/Momo";
-import { Card } from "@material-ui/core";
+
+import MyGrid from "../components/MyGrid";
 
 export default function Physics() {
   function displayMainPhysics() {
     return (
       <div className="mainClass" style={{backgroundColor: "#354e01"}}>
-        <HashRouter>
-          <Card>
-            <div className="body">
-              <div className="entry">
-                <NavLink to="/kinematics" style={{color: "black"}}>Kinematics</NavLink>
-                <p style={{width: "80%"}}>
-                  This unit will cover everything from vector addition to 2-body kinematics problems. 
-                </p>
-                
-              </div>
-              <div className="entry">
-              <NavLink to="/dynamics" style={{color: "black"}}>Dynamics</NavLink>
-              <Route path="/dynamics" component={Dynamics}/>
-              </div>
-              <div className="entry">
-                <NavLink to="/energy" style={{color: "black"}}>Energy</NavLink>
-                <Route path="/energy" component={Energy}/>
-              </div>
-              <div className="entry">
-                <NavLink to="/momentum" style={{color: "black"}}>Momentum</NavLink>
-                <Route path="/momentum" component={Momo}/>
-              </div>
-
-            </div>
-            
-          </Card>
-        </HashRouter>
+        <MyGrid style={{background:"#354e01",  backgroundColor: "#354e01"}} className="testertester"></MyGrid>
       </div>
     );
   }
