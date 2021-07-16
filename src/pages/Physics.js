1c79aa9998099ebfcfb2f5d74f629ab676ff1c51
Charles Daab <charlesjdaab@gmail.com>
2021-07-16 13:08:21 -0400

    removed kinematics routepath to try actual link

--- src/pages/Physics.js
+++ src/pages/Physics.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a81bee1deb44b91ed4a5884f84bc92ec5d876490d934a0565ef8de2f4b858a9
-size 2063
+oid sha256:2c44466f1a369814e776e6854ddea2777de66cee078795e81f4c67c7ad9c8382
+size 1780
