beb35f58d9ef8037f33141420c70baf176d76868
Charles Daab <charlesjdaab@gmail.com>
2021-07-16 12:43:17 -0400

    te

--- src/pages/Physics.js
+++ src/pages/Physics.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./styles/Physics.css";
-import Header from "../components/Header/Header";
 import {
   Route,
   HashRouter,
+  NavLink,
 } from "react-router-dom";
 
 import Kinematics from "../containers/Kinematics";
@@ -11,13 +11,19 @@ import Dynamics from "../containers/PhysicsContainers/Dynamics";
 import Energy from "../containers/PhysicsContainers/Energy";
 import Momo from "../containers/PhysicsContainers/Momo";
 import Rotation from "../containers/PhysicsContainers/Rotation";
+import { Card } from "@material-ui/core";
 
 export default function Physics() {
   function displayMainPhysics() {
     return (
       <div className="mainClass" style={{backgroundColor: "#354e01"}}>
         <HashRouter>
-        <Header></Header>
+          <Card>
+            <div className="body">
+            <NavLink to="/kinematics" style={{color: "white"}}>Kinematics</NavLink>
+            </div>
+          </Card>
+        
         <div className="content">
           <Route path="/kinematics" component={Kinematics}/>s
           <Route path="/dynamics" component={Dynamics}/>
