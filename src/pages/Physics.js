e0dacd50d8a7e5ed5e9d16bc7caf3ed0bce2e7b0
Charles Daab <charlesjdaab@gmail.com>
2021-07-16 12:53:43 -0400

    put navlink and route path in own div

--- src/pages/Physics.js
+++ src/pages/Physics.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a24fefef876bcd42f7a39a096406c55edb875c6f921ffc6d1e2b11483ca763ed
-size 1322
+oid sha256:88808c2221e29f644cb5df999a6364e8a8539b368dd359de38721c6af101ee64
+size 1829
