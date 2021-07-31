3fb624ba90325f4c1af608e69247125294962275
Charles Daab <charlesjdaab@gmail.com>
2021-07-31 08:03:27 -0400

    physics styling

--- src/pages/PhysicsPages/IntroPage.js
+++ src/pages/PhysicsPages/IntroPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:448b1d2d3f31e641c977e579e147b0c4e34fcb2bc43e49faaa4aa0ef7541ff9e
-size 1228
+oid sha256:def60bb0b2b9585dee2453ba477efa49725a523697cb850a4a9095979e62a80f
+size 1264
---
+++ src/pages/PhysicsPages/SimpleMotionPage.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:48a9f8b5ac38f4c8946a2ebce8ea04bb22da4ef2de9659a0b3088745047b9cee
+size 419
