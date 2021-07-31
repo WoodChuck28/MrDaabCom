ba476b8511170c5a400c1c735dbf0c614a401da2
Charles Daab <charlesjdaab@gmail.com>
2021-07-31 10:09:29 -0400

    SimpleMotionPage

--- src/pages/PhysicsPages/SimpleMotionPage.js
+++ src/pages/PhysicsPages/SimpleMotionPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48801fdf461323d8633ba785d8dd793952d400c5294277bff79ff638d4d90605
-size 1177
+oid sha256:5684c563ad576ecb831e422102b1016546aedacc1bb9be69343601debc6ea479
+size 1181
