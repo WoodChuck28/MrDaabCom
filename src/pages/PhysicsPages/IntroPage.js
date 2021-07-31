cd8bdb6ab59e64a464ee8305f6aa4b966a1d9b47
Charles Daab <charlesjdaab@gmail.com>
2021-07-31 07:50:21 -0400

    h3 and p tags on intro

--- src/pages/PhysicsPages/IntroPage.js
+++ src/pages/PhysicsPages/IntroPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa375bf53232133558b0538dbcd8752b8413fa8904aefd371fbd8f6278e7b5ef
-size 358
+oid sha256:4cf90b364b9e43ed134b6b39493699b4d6480a8a4000d276bfef4dc5a73d3671
+size 990
