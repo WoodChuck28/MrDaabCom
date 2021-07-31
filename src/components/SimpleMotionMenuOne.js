ff931c7d394cf380f9ba624d555461b73483cb37
Charles Daab <charlesjdaab@gmail.com>
2021-07-31 08:32:25 -0400

    SimpleMotionPage

---
+++ src/components/SimpleMotionMenuOne.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f4b1edc23f15c5248e93b3754bc148429a235999e188d95b03385bd59ec5e110
+size 1024
--- src/pages/PhysicsPages/SimpleMotionPage.js
+++ src/pages/PhysicsPages/SimpleMotionPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e83c2b7db6b36ed405beecdb05ab070fb73d28f99fe8db3d2e1cc58bd8e806a
-size 1783
+oid sha256:da02ddfa06b4c9ec8e12c2b09c3d2a2b996490772a1cf9a597fbc3b58e3cba17
+size 1813
