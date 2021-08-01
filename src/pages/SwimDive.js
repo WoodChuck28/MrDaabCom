24b7cc3f090315b6b672dcbe7b02cdebc72b1692
Charles Daab <charlesjdaab@gmail.com>
2021-08-01 08:43:47 -0400

    added notes and practice

--- src/pages/SwimDive.js
+++ src/pages/SwimDive.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e9fd656ccd2fa507eb629982e14ed299643beb4713c6ae0e78cb4cb9da72691
-size 1504
+oid sha256:5c6767965d760e16e12e2fda2d73ba4e02468741d0f065afe22c37149a1fd47d
+size 1552
