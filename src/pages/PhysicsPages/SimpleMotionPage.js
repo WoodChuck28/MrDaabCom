db1425737c9f6a61bc4a0c3d227f74e3a0b803db
Charles Daab <charlesjdaab@gmail.com>
2021-08-01 10:02:58 -0400

    Practice Sheet

--- src/pages/PhysicsPages/SimpleMotionPage.js
+++ src/pages/PhysicsPages/SimpleMotionPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41dff4a18e3ee310c291816d5dc80484fc8b34195ebd0b775efcefef8fdb7733
-size 2478
+oid sha256:222279e46a8541158a4ad38690055238158edb622df0a9d6a60a1087d074404f
+size 2457
