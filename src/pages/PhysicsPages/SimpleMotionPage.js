794878f86d648b364671be79d118f059c1abc6c9
Charles Daab <charlesjdaab@gmail.com>
2021-08-07 07:16:05 -0400

    Stance image and reformat

--- src/pages/PhysicsPages/SimpleMotionPage.js
+++ src/pages/PhysicsPages/SimpleMotionPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f35493ce4d9eae288cb53e120d9d0bdad973d1a278e4ee1fb84c27dcfcb734f5
-size 2765
+oid sha256:5a1c5edc13890f6e3010c9848d1eaccabbebcedd2771a43413bb5bad9b90ff1c
+size 2852
