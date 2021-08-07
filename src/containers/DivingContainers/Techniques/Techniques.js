ef5ab9fb3ba0afd9903e4b416f651a61a57b9ce0
Charles Daab <charlesjdaab@gmail.com>
2021-08-07 07:19:50 -0400

    Stance image and reformat

--- src/containers/DivingContainers/Techniques/Techniques.js
+++ src/containers/DivingContainers/Techniques/Techniques.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd8e125ba9478ccb79a85d2038585b906bea3fefc81cb294a67605c109bd5be6
-size 1337
+oid sha256:09ccb7d58f60a211d9c90a2994c8c929a3c0fb7fc1f2dde54f9cef53f6a737d3
+size 1370
